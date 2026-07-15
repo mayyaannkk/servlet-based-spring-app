564a539100221dfd42ccf354f86a5caa98f34cc2
mayyaannkk <mayankpant.dev@gmail.com>
2026-07-15 14:19:05 +0530

    basic servlet and tomcat

---
+++ src/main/java/com/mayank/ApplicationLauncher.java
@@ -0,0 +1,22 @@
+package com.mayank;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+
+public class ApplicationLauncher {
+    static void main() throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context context = tomcat.addContext("", null);
+        Wrapper wrapper = Tomcat.addServlet(context, "myFirstServlet", new MyFirstServlet());
+
+        wrapper.setLoadOnStartup(1);
+        wrapper.addMapping("/*");
+
+        tomcat.start();
+    }
+}
