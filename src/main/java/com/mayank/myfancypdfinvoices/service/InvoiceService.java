14bba4f78b581f24c87827ca4c8ffd026a0ee3e7
mayyaannkk <mayankpant.dev@gmail.com>
2026-07-17 18:19:29 +0530

    added user model and user validation

--- src/main/java/com/mayank/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/mayank/myfancypdfinvoices/service/InvoiceService.java
@@ -1,6 +1,8 @@
 package com.mayank.myfancypdfinvoices.service;
 
+import com.mayank.myfancypdfinvoices.context.Application;
 import com.mayank.myfancypdfinvoices.model.Invoice;
+import com.mayank.myfancypdfinvoices.model.User;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -14,6 +16,11 @@ public class InvoiceService {
     }
 
     public Invoice create(String userId, Integer amount) {
+        User user = Application.userService.findById(userId);
+        if(user == null) {
+            throw new IllegalStateException();
+        }
+
         Invoice invoice = new Invoice(userId, amount, "http://www.africau.edu/images/default/sample.pdf");
         System.out.println(invoices);
         invoices.add(invoice);
