81e0d74452303d98e935c04f9ee83ebb001a10e8
mayyaannkk <mayankpant.dev@gmail.com>
2026-07-19 22:38:00 +0530

    adding spring MVC, replacing our custom servlet with Spring MVC dispatcher servlet

--- src/main/java/com/mayank/myfancypdfinvoices/ApplicationLauncher.java
+++ src/main/java/com/mayank/myfancypdfinvoices/ApplicationLauncher.java
@@ -1,10 +1,15 @@
 package com.mayank.myfancypdfinvoices;
 
+import com.mayank.myfancypdfinvoices.context.MyFancyPdfInvoicesApplicationConfiguration;
 import com.mayank.myfancypdfinvoices.web.MyFancyPdfInvoicesServlet;
+import jakarta.servlet.ServletContext;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class ApplicationLauncher {
     static void main() throws LifecycleException {
@@ -13,11 +18,27 @@ public class ApplicationLauncher {
         tomcat.getConnector();
 
         Context context = tomcat.addContext("", null);
-        Wrapper wrapper = Tomcat.addServlet(context, "myFirstServlet", new MyFancyPdfInvoicesServlet());
+
+
+        WebApplicationContext appCtx = createApplicationContext(context.getServletContext());
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(appCtx);
+
+
+        Wrapper wrapper = Tomcat.addServlet(context, "dispatcherServlet", dispatcherServlet);
 
         wrapper.setLoadOnStartup(1);
         wrapper.addMapping("/*");
 
         tomcat.start();
+    }
+
+    private static WebApplicationContext createApplicationContext(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(MyFancyPdfInvoicesApplicationConfiguration.class);
+        ctx.setServletContext(servletContext);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+        return ctx;
     }
 }
